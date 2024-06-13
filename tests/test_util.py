c7882d457279d57a5683c6d4264fe111c5f19e52
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2024-06-13 10:42:33 -0300

    test: update test_utils.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -18,10 +18,10 @@ def test_move_file(tmp_path):
     src = tmp_path / "source.txt"
     dst = tmp_path / "destination.txt"
     with open(src, "w") as f:
-        f.write("Hello, world!")
+        f.write("Heello, world!")
 
     move_file(src, dst)
     assert os.path.exists(dst)
     assert not os.path.exists(src)
     with open(dst, "r") as f:
-        assert f.read() == "Hello, world!"
+        assert f.read() == "Heello, world!"
